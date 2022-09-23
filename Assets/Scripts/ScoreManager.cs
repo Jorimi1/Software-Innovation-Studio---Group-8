008d08a58b14ec02893d92abfbad9338416c611d
Yunwei-Zhang <62420557+Yunwei-Zhang@users.noreply.github.com>
2022-09-23 11:03:44 +1000

    most done
    Level1 base + Boss room + flag + dialogue + playerprefs + boss level base

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -3,38 +3,84 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour
-{ 
+{
     public static ScoreManager instance;
     public Image image1;
     public Image image2;
     public Image image3;
-    public static float Score { set;get; }
-    double collectedNumber = 0;
-    
+    public static float Score { set; get; }
+    public static int collectedNumber = 0;
+
     void Start()
     {
+        //disable all images
         image1.enabled = false;
         image2.enabled = false;
         image3.enabled = false;
-        if(instance == null){
-            instance= this;
+
+        //get apple value in levelboss
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        if (sceneName == "LevelBoss")
+        {
+            collectedNumber = PlayerPrefs.GetInt("collectedapple");
+            if (collectedNumber == 1)
+            {
+                image1.enabled = true;
+            }
+            else if (collectedNumber == 2)
+            {
+                image2.enabled = true;
+            }
+            else if (collectedNumber == 3)
+            {
+                image3.enabled = true;
+            }
+            
         }
+        
+        PlayerPrefs.SetInt("collectedapple", 0);
+        //instance
+        if (instance == null)
+            {
+                instance = this;
+            }
+
+        
+
+
     }
 
     // Update is called once per frame
-    public void collectItem(){
+    public void collectItem()
+    {
         collectedNumber++;
-        
-        if(collectedNumber == 1){
+
+        if (collectedNumber == 1)
+        {
             image1.enabled = true;
         }
-        else if(collectedNumber == 2){
+        else if (collectedNumber == 2)
+        {
             image2.enabled = true;
         }
-        else if(collectedNumber == 3){
+        else if (collectedNumber == 3)
+        {
             image3.enabled = true;
+        }
+
+        //set apple value in level1
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        if (sceneName == "Level1")
+        {
+            PlayerPrefs.SetInt("collectedapple", collectedNumber);
+            Debug.Log(PlayerPrefs.GetInt("collectedapple"));
         }
     }
 }
